08b248527d33741a2a11fa62b9a0b31f6152a639
wucong <q1875486458@gmail.com>
2021-05-06 13:35:09 +0800

    update

--- ubuntu-20/ubuntu20.sh
+++ ubuntu-20/ubuntu20.sh
@@ -447,3 +447,16 @@ sudo add-apt-repository ppa:daniel.p/dpscreenocr
 sudo apt-get update
 sudo apt install dpscreenocr
 sudo apt install tesseract-ocr-eng 
+
+
+# sublimetext3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt-get install sublime-text
+
+# crystal
+curl -fsSL https://crystal-lang.org/install.sh | sudo bash
+
+# init xdotool-ext
+
